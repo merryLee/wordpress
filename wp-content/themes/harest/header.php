7435c6acbc9d903012d67aefef3e92797dea8902
seonmin <ismpink@daum.net>
2017-03-24 12:55:31 +0900

    pre-header 수정.

--- wp-content/themes/harest/header.php
+++ wp-content/themes/harest/header.php
@@ -33,12 +33,16 @@
 		<div class="wrap-safari">
 		<div id="pre-header-core" class="main-navigation">
   
-			<?php if ( has_nav_menu( 'pre_header_menu' ) ) : ?>
+<!--			<?php if ( has_nav_menu( 'pre_header_menu' ) ) : ?>
 			<?php wp_nav_menu( array( 'container_class' => 'header-links', 'container_id' => 'pre-header-links-inner', 'theme_location' => 'pre_header_menu' ) ); ?>
-			<?php endif; ?>
-
+			<?php endif; ?>		-->
 			<?php /* Social Media Icons */ thinkup_input_socialmediaheaderpre(); ?>
 
+			<div class="pre-header-info">
+			<i class="fa fa-phone" aria-hidden="true"></i> Tel. +82 31­-237-3259  │  
+			<i class="fa fa-envelope" aria-hidden="true"></i> pacificways@naver.com
+			</div>
+			
 		</div>
 		</div>
 		</div>
